486443295bfa624fbf3c127cb3a7a505915cb537
Jackson Wiebe <jacksonwiebe@me.com>
2022-01-21 12:53:12 -0700

    Working Template 1

---
+++ Control-App/app/src/main/java/com/example/heliographapp/SettingsFragment.java
@@ -0,0 +1,18 @@
+package com.example.heliographapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+public class SettingsFragment extends Fragment {
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
